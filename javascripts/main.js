7eac63fcf779b74c6a61df4c98ad95706447803f
John Fleming <johndeclanfleming@gmail.com>
2020-02-25 20:25:19 -0600

    added bread radio buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('hi');
+// div in html - id="bread-container"
+// printToDom function in utils
+// breadData will have array of bread objects, export a getBreads function
+// each bread object = type, color, price, id
+// breads.js component - with domString builder (find something nice in bootstrap) use getBreads function
+// import breads component into main
+// event listener on radio button - call breadData.setSelectedBread(breadId);
+// setSelectedBread = assign a variable (selectedBreadId) to the id passed in (console.log the id)
+
+import breadButtons from './components/bread.js';
+
+const init = () => {
+    breadButtons.makeBreadRadioButtons();
+};
+
+init();
